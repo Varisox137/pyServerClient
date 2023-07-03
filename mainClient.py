440b5154665c6d68075c1840061abb0d5f7fb770
Varisox137 <Varisox137@163.com>
2023-07-03 21:25:48 +0800

    Hashed registration & testing

--- mainClient.py
+++ mainClient.py
@@ -1,43 +1,46 @@
 from concurrent.futures import ThreadPoolExecutor
-import os
 from time import sleep
 
 # import bwpgrb.bwpMain
 
-CLIENT_VERSION='230517'
+# real globals, should never be changed
+CLIENT_VERSION='230703'
 URL='https://n6944f2933.imdo.co/'
 
 MAX_WK=5 # TPE max worker
+
+# the capitalized pseudo-globals defined closely before methods are actually globally-working objects
 
 def dialog(command:str,method:str,data:dict=None):
 	import urllib.request as rq
 	import urllib.parse as ps
 
 	def urldecode(s:str):
-		# no need to set the split() param: maxsplit, as the inner dict ['data'] has been url-encoded, '&'->'%26'
+# no need to set the split() param: maxsplit, as res_dict['data'] should have already been url-encoded, e.g. '&'->'%26'
 		d={}
 		if s: d.update(list(map(
-				lambda x:(ps.unquote_plus((s:=x.split('='))[0]),
-				          ps.unquote_plus(s[1])),
+				lambda x:(ps.unquote_plus((m:=x.split('='))[0]),
+				          ps.unquote_plus(m[1])),
 				s.split('&'))))
 		return d
 
-	HEADERS={'Connection': 'keep-alive',
+	headers={'Connection': 'keep-alive',
 	         'Content-Type': 'text/plain'}
 	if data is None: data=dict()
 	data['command']=command
 	data_enc=ps.urlencode(data).encode('utf-8')
-	request=rq.Request(url=URL,data=data_enc,headers=HEADERS,method=method.upper()) # http method should be upper case
+	request=rq.Request(url=URL,data=data_enc,headers=headers,method=method.upper()) # http method should be upper case
 	response=rq.urlopen(request).read().decode()
 	res_dict=urldecode(response)
 	res_dict['data']=urldecode(res_dict['data'])
 	return res_dict
 
-USR=''
+USR='' # username
 def log_or_reg():
+	import os # for ENVIRON and SYSNAME only
 	global USR
 	loc_usrs=[]
-	base={'nt':'LOCALAPPDATA','posix':'HOME'}
+	base={'nt':'LOCALAPPDATA','posix':'HOME'} # the base location for local config to store
 	# try to get local config first
 	usr=''
 	pwd=''
@@ -64,19 +67,20 @@ def log_or_reg():
 				(usr,pwd)=loc_usrs[0]
 		else: print('\nWill log in or register manually...'); sleep(0.7)
 	else: print('\nNo local config found...'); sleep(0.7)
-	# now, either no local config found, or (usr,pwd) already chosen from local, or just choose to log in manually
+# now, either no local config found, or (usr,pwd) already chosen from local, or the user chooses operate manually
 	from hashlib import sha3_256
-	if not (usr or pwd):
+	from datetime import datetime as dt
+	if not (usr or pwd): # not from local config choice
 		reg_key=input('\nEnter registration code, or skip by entering nothing...?=\n')
 		if reg_key:
-			res=dialog(command='chk_reg',method='post',data={'key':(reg_key if reg_key.isalnum() else '')})
+			res=dialog(command='chk_reg',method='post',data={'key':reg_key})
 			msg=res['message']
 		else: msg='Notice: Registration skipped.'
 		print(msg); sleep(0.7)
-		if msg.startswith('Successful'): # code pass, create new user
+		if msg.startswith('Successful'): # code correct, starts registration process
 			print('\nBegin new user registration...'); sleep(0.7)
 			while True: # guarantee a valid new username
-				usr=input('\nEnter new username :=\n')
+				usr=input('\nEnter new username :=\n') # all checks done by server
 				res=dialog(command='chk_new_usn',method='post',data={'username':usr})['message']
 				print(msg:=res['message'])
 				if msg.startswith('Successful'): break
@@ -86,17 +90,25 @@ def log_or_reg():
 					print('Password unmatch !')
 				else: # reg to server
 					USR=usr
-					res=dialog(command='register',method='post',data={'username':usr,'password':pwd})
+					crpt=sha3_256(pwd.encode('utf-8')) # construct an encrypter, pwd -> hash
+					# Note that registration sends only hash of pwd, without timestamp-initialization
+					res=dialog(command='register',method='post',data={'username':usr,'pwd_hash':crpt.hexdigest()})
 					print(res['message'])
 					break
-		else: # login an existing user
+		else: # login an existing user, manually
 			print('\nBegin normal login process...'); sleep(0.7)
 			while True: # has to check manual login
-				crpt=sha3_256()
 				usr=input('\nEnter username :=\n')
 				pwd=input('Enter password :=\n')
-				crpt.update(pwd.encode('utf-8'))
-				res=dialog(command='login',method='post',data={'username':usr,'pwd_hash':crpt.hexdigest()})
+				crpt=sha3_256(str(
+					ts:=int(dt.now().timestamp())
+				).encode('utf-8'))  # initializes with current timestamp (seconds), before sending to server
+				crpt.update(
+					(ex:=sha3_256(pwd.encode('utf-8')).hexdigest()).encode('utf-8')
+				) # pwd -> hash -> timestamped-hash
+				hash_final=crpt.hexdigest()
+				if T: print('\n***hash check***\nts',ts,'ex',ex,'\nfinal =',hash_final,'\n')
+				res=dialog(command='login',method='post',data={'username':usr,'hash':hash_final})
 				print(msg:=res['message'])
 				if msg.startswith('Caution'):
 					USR=usr='admin'
@@ -104,13 +116,21 @@ def log_or_reg():
 				elif msg.startswith('Successful'):
 					USR=usr
 					break
-	else: # local config auto login
-		crpt=sha3_256(pwd.encode('utf-8'))
-		res=dialog(command='login',method='post',data={'username':usr,'pwd_hash':crpt.hexdigest()})
+	else: # choice from local config, automatic login
+		crpt=sha3_256(str(
+			ts:=int(dt.now().timestamp())
+		).encode('utf-8'))  # initializes with current timestamp (seconds), before sending to server
+		crpt.update(
+			(ex:=sha3_256(pwd.encode('utf-8')).hexdigest()).encode('utf-8')
+		) # pwd -> hash -> timestamped-hash
+		hash_final=crpt.hexdigest()
+		if T: print('\n***hash check***\nts',ts,'ex',ex,'\nfinal =',hash_final,'\n')
+		res=dialog(command='login',method='post',data={'username':usr,'hash':hash_final})
 		print(msg:=res['message'])
 		# local config should guarantee user existence in server, with the correct password
 		assert msg.startswith('Successful'),'LocalConfigLoginError'
 		USR=usr
+	del crpt,dt,sha3_256 # release
 	# finally, refresh local user and overwrites config
 	if usr!='admin': # admin mustn't be recorded into config
 		if usr not in list(map(lambda x:x[0],loc_usrs)):
@@ -119,21 +139,21 @@ def log_or_reg():
 		for each in loc_usrs:
 			config.writelines((each[0]+'\n',each[1]+'\n'))
 
-ALIVE=True
+ALIVE=True # program keep-alive
 def keep_conn(interval:int|float):
 	while ALIVE:
 		dialog(command='keep',method='post',data={'username':USR})
 		sleep(interval)
 
-CUR_RM=''
-CMD_LS=[]
+CUR_RM='' # the current room the user is now in
+CMD_LS=[] # available command list
 def command_cycle():
 	global REC,CUR_RM,CMD_LS
 	print('\nNow you\'ve entered the command cycle.')
 	print('Getting full command list...'); sleep(0.7)
 	res=dialog(command='get_cmd_ls',method='get')
 	print(res['message'])
-	cmd_ls=res['data']['commands'].split() # command list
+	CMD_LS=res['data']['commands'].split() # command list
 	cmd_des=res['data']['description'] # command description
 	lobby=True
 	while True:
@@ -207,10 +227,11 @@ def command_cycle():
 								REC[1][CUR_RM]+=len(rm_msg.split('\n'))
 						case _:
 							print('Command incorrect !')
-				elif cmd not in cmd_ls: print('Command incorrect!')
+				elif cmd not in CMD_LS: print('Command incorrect!')
 
-REC=[0,{}] # chat history position record, in the form of [pub,{rid:rm,rid:rm...}]
-def msg_refr(interval:int|float):
+REC=[0,{}] # chat history record (the position of already-read/last-received messages)
+# in the form of [pub_rec,{rid:rm_rec,rid:rm_rec...}], with types: rid->str, rec->int
+def msg_refr(interval:int|float): # refreshes, if to receive new chat messages
 	global REC
 	while ALIVE:
 		if CUR_RM:
@@ -230,7 +251,7 @@ def msg_refr(interval:int|float):
 			REC[0]+=len(pub_msg.split('\n'))
 		sleep(interval)
 
-EXECUTOR=ThreadPoolExecutor(max_workers=MAX_WK)
+EXECUTOR=ThreadPoolExecutor(max_workers=MAX_WK) # multi-threading
 def terminate():
 	global ALIVE
 	ALIVE=False
@@ -246,28 +267,30 @@ def admin_mode():
 
 GAMES=('Splendor',) # module name, available offline-mode games, still developing & adding
 if __name__=='__main__':
-	T=bool(input('test mode ?')=='s'); sleep(0.7)
+	T=bool(input('test mode ?')); sleep(0.7)
 	print('\nClient version : '+CLIENT_VERSION+'\n'); sleep(0.7)
 	try:
-		print(dialog(command='try',method='post')['message']) # check if server is online; btw res should have ['data']=={}
+		print(dialog(command='try',method='post')['message']) # check if server is online; res should have ['data']=={}
 	except Exception:
 		from traceback import print_exc
 		print_exc()
 		sleep(1)
-		print("\nOops! Seems the Server isn't online...")
+		print("\nOops! Seems the server isn't online......or maybe an error just occurred ?")
 		print('Starting offline mode......\n'); sleep(0.7)
 		if not input('press enter to start a mini-game ?'):
 			game_name=input('enter the name of the game you want to play ?=')
 			if game_name in GAMES:
-				from importlib import import_module
-				game=import_module(game_name)
+				from importlib import import_module as im
+				game=im(game_name)
+				del im
 				game.game_main()
-		input('Program finished, press enter to quit......\n')
+		input('\nProgram finished, press enter to quit......\n')
 	else: # server online
 		print("\nWelcome to Varisox137's server! (still improving yet)"); sleep(0.7)
 		log_or_reg() # login or registration, USR set
-		sleep(0.7)
+		sleep(1)
 		if USR=='admin':
+			print('\nTBD......')
 			EXECUTOR.submit(admin_mode)
 		elif not T:
 			EXECUTOR.submit(command_cycle)
